f736c3d59ef6df8f0ef7e48b677782617838c09b
Minh Tran <thminh_1@spsvietnam.vn>
2020-11-23 15:20:05 +0700

    Initialize project

---
+++ src/test/java/vn/minhtran/sbw/SpringBootBpmn2WfApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.minhtran.sbw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBpmn2WfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

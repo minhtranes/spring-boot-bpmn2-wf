70e6f3bf430d6494430208e990ac75610de362ac
Minh Tran <wisekingtran@gmail.com>
2021-01-13 10:32:06 +0700

    Isolate the history application

---
+++ spring-boot-bpmn2-history/README.md
@@ -0,0 +1,4 @@
+# Exposed API
+## Query process information
+## Query process instance information
+## Query process instance detail
---
+++ spring-boot-bpmn2-history/src/main/java/vn/minhtran/sbw/ActivitiHistoryApplication.java
@@ -0,0 +1,13 @@
+package vn.minhtran.sbw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiHistoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiHistoryApplication.class, args);
+	}
+
+}
